2db2390457a0cd8cd8f9da60807e2504573954d6
kashelton17 <kashelton17@gmail.com>
2021-08-19 11:41:25 -0500

    testing for heroku

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,7 +4,7 @@ const Workout = require("../models/workout.js");
 
 router.get("/api/workouts", async (req, res) => {
     try {
-        await Workout.aggregate([{ "$addFields": {"totalDuration": {$sum: "$exercises.duration" },"totalWeight": {$sum: "$exercises.weight" }}}]).then(each => {
+        await Workout.aggregate([{ "$addFields": {"totalDuration": {$sum: "$exercises.duration" },"totalWeight": {$sum: "$exercises.weight" }, "totalReps": {$sum: "$exercises.reps" },"totalSets": {$sum: "$exercises.sets" }}}]).then(each => {
             res.json(each);
             console.log(each)
         })
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/tracker", {
+    process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true,
