f703e91db6589a476ce8969bcf9ac9cf57ca609a
kashelton17 <kashelton17@gmail.com>
2021-08-19 14:27:59 -0500

    fixing range route to display bar and line chart accuratley

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const mongoose = require('mongoose');
-const Workout = require("../../models/workout");
+const Workout = require("../models/workout");
 
 router.get("/api/workouts", async (req, res) => {
     try {
-        await Workout.aggregate([{ "$addFields": {"totalDuration": {$sum: "$exercises.duration" },"totalWeight": {$sum: "$exercises.weight" }, "totalReps": {$sum: "$exercises.reps" },"totalSets": {$sum: "$exercises.sets" }}}]).then(each => {
+        await Workout.aggregate([{ "$addFields": {"totalDuration": {$sum: "$exercises.duration" },"totalWeight": {$sum: "$exercises.weight" }, "totalReps": {$sum: "$exercises.reps" },"totalSets": {$sum: "$exercises.sets" }}}]).sort({day:-1}).then(each => {
             res.json(each);
             console.log(each)
         })
@@ -26,8 +26,9 @@ router.post('/api/workouts', async (req, res) => {
 
 router.get("/api/workouts/range", async (req, res) => {
     try {
-       await Workout.find({}).then((dbWorkout) => {
+        await Workout.aggregate([{ "$addFields": {"totalDuration": {$sum: "$exercises.duration" },"totalWeight": {$sum: "$exercises.weight" }, "totalReps": {$sum: "$exercises.reps" },"totalSets": {$sum: "$exercises.sets" }}}]).limit(7).then((dbWorkout) => {
             res.json(dbWorkout);
+            console.log(dbWorkout)
         })
     } catch (err) {
       res.status(400).json(err);
@@ -37,12 +38,14 @@ router.get("/api/workouts/range", async (req, res) => {
 router.put("/api/workouts/:id", async (req, res) => {
     try {
         await Workout.findByIdAndUpdate(
-            { _id: req.params.id }, { exercises: req.body }
+            { _id: req.params.id }, {$push: { exercises: req.body }}
           ).then((dbWorkout) => {
             res.json(dbWorkout);
+            console.log(dbWorkout)
           })
     } catch(err) {
-      res.status(400).json(err);
+        console.log(err)
+        res.status(400).json(err);
     };
 });
 
