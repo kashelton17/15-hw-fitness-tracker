51847420ac89f2f30cdcbc792c1cb16130021a7f
kashelton17 <kashelton17@gmail.com>
2021-08-18 20:31:03 -0500

    adding api and html routes to get and post data

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
-
 const app = express();
 
 app.use(logger("dev"));
@@ -15,4 +13,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+app.use(require("./routes/html.js"));
+app.use(require("./routes/api.js"))
+
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
